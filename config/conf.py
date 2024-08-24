1f5ec6eb0dffc7712776eb4f478ec9462d110c32
Juan Carlos Ordonez <jordonez@thefives.com.mx>
2024-08-24 10:22:56 -0500

    feat(configuration): Se agrega el archivo base de configuración

---
+++ config/conf.py
@@ -0,0 +1,18 @@
+import os
+import configparser
+
+class AppSettings(object):
+    def __init__(self) -> None:
+        self.__path_loc = os.path.dirname(os.path.abspath(__file__))
+        self.__config = configparser.ConfigParser()
+        self.__config.read(os.path.join(self.__path_loc, 'conf.cfg'))
+
+    @property
+    def project_path(self):
+        return self.__config.get('core', 'project_path')
+
+    @property
+    def base_url(self):
+        return self.__config.get('urls', 'base_url')
+    
+    
