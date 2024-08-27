138be1179199a36394748a225e19dbe2f4c1d8fa
Juan Carlos Ordonez <jordonez@thefives.com.mx>
2024-08-27 10:35:41 -0500

    fix(extractor): Se corrige el error del extractor que obtiene el xml, para guardarlo en binario y corregir errores de codificación

--- scr/utils/extractor.py
+++ scr/utils/extractor.py
@@ -6,14 +6,14 @@ import os
 def data_extractor(url: str, filebasename):
     settings = AppSettings()
     response = requests.get(url)
-    data = response.text
-
-    file_path = os.path.join(settings.project_path, 'data/ext', filebasename)
-    os.makedirs(os.path.join(settings.project_path, 'data/ext'), exist_ok=True)
     if response.status_code != 200:
         response.raise_for_status()
+        
+    data = response.content
+    file_path = os.path.join(settings.project_path, 'data/ext', filebasename)
+    os.makedirs(os.path.join(settings.project_path, 'data/ext'), exist_ok=True)
 
-    with open(file_path, 'w') as f:
+    with open(file_path, 'wb') as f:
         f.write(data)
         
     return file_path
