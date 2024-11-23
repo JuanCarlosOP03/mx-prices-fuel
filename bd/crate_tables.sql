72e7096e2ac46533aa045215b9ad33ee4a55f31a
Juan Carlos Ordonez <Carl_25_8@hotmail.com>
2024-11-23 08:52:02 -0500

    feat(bd): Se genera un solo script para la creación de las 3 tablas donde se guarda la información descargada

--- bd/crate_tables.sql
+++ bd/crate_tables.sql
@@ -1,3 +1,38 @@
+
+DROP TABLE IF EXISTS "mx_prices_fuel"."public"."places" CASCADE;
+CREATE TABLE "mx_prices_fuel"."public"."places" (
+    "place_id" INT PRIMARY KEY NOT NULL,
+    "cre_id" VARCHAR(25) NOT NULL,
+    "longitude" DOUBLE PRECISION NOT NULL, 
+    "latitude" DOUBLE PRECISION NOT NULL,  
+    "place_name" VARCHAR(150)                
+);
+
+COMMENT ON TABLE "mx_prices_fuel"."public"."places" IS 'Tabla que almacena información sobre estaciones de combustible';
+COMMENT ON COLUMN "mx_prices_fuel"."public"."places"."place_id" IS 'Identificador único de la estación de servicio';
+COMMENT ON COLUMN "mx_prices_fuel"."public"."places"."cre_id" IS 'Identificador del permiso';
+COMMENT ON COLUMN "mx_prices_fuel"."public"."places"."longitude" IS 'Longitud de la estación de servicio en grados';
+COMMENT ON COLUMN "mx_prices_fuel"."public"."places"."latitude" IS 'Latitud de la estación de servicio en grados';
+COMMENT ON COLUMN "mx_prices_fuel"."public"."places"."place_name" IS 'Nombre de la estación de servicio';
+
+CREATE UNIQUE INDEX idx_cre_id ON "mx_prices_fuel"."public"."places" ("cre_id");
+CREATE INDEX idx_location ON "mx_prices_fuel"."public"."places" ("longitude", "latitude");
+
+DROP TABLE IF EXISTS "mx_prices_fuel"."public"."prices" CASCADE;
+CREATE TABLE "mx_prices_fuel"."public"."prices" (
+    "place_id" INT NOT NULL REFERENCES "mx_prices_fuel"."public"."places" ("place_id"),  
+    "fuel_type" VARCHAR(30) NOT NULL,
+    "type_product" VARCHAR(30) NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL
+);
+
+COMMENT ON COLUMN "mx_prices_fuel"."public"."prices"."place_id" IS 'Identificador de la estación de servicio';
+COMMENT ON COLUMN "mx_prices_fuel"."public"."prices"."fuel_type" IS 'Tipo de combustible: gasolina o diésel';
+COMMENT ON COLUMN "mx_prices_fuel"."public"."prices"."type_product" IS 'Tipo de producto: premium, magna o diésel';
+COMMENT ON COLUMN "mx_prices_fuel"."public"."prices"."price" IS 'Precio del combustible en la estación de servicio';
+
+CREATE INDEX idx_place_id ON "mx_prices_fuel"."public"."prices" ("place_id");
+
 DROP TABLE IF EXISTS "mx_prices_fuel"."public"."places_details" CASCADE;
 CREATE TABLE "mx_prices_fuel"."public"."places_details" (
     "turn_code" VARCHAR(10) NOT NULL,
