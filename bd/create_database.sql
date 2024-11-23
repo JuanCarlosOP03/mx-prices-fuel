5b0839ac951d60638a4718be088c82a396136c50
Juan Carlos Ordonez <Carl_25_8@hotmail.com>
2024-11-23 08:57:39 -0500

    docs(sql): Se agrega comentarios a las consultas

--- bd/create_database.sql
+++ bd/create_database.sql
@@ -1,10 +1,13 @@
 
+--Creación de la base de datos
 CREATE DATABASE mx_prices_fuel
 WITH 
     ENCODING = 'UTF8',     
     LC_COLLATE = 'es_ES.UTF-8',
     LC_CTYPE = 'es_ES.UTF-8';  
 
+--Creación del Usuario que va a insertar datos en la BD, se puede cambiar.
 CREATE USER etl_user WITH ENCRYPTED PASSWORD '1Pass=0309';
+
 GRANT ALL PRIVILEGES ON DATABASE mx_prices_fuel TO etl_user;
 COMMENT ON DATABASE mx_prices_fuel IS 'Base de datos para la gestión de precios de combustible en México';
